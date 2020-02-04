cac214b9364b8ea40380fe323d56b5aa0b672031
thenry3 <43651306+thenry3@users.noreply.github.com>
2020-02-03 18:09:04 -0800

    initalizze

---
+++ sendtexts.py
@@ -0,0 +1,18 @@
+# from twilio.rest import Client
+
+
+# # Your Account Sid and Auth Token from twilio.com/console
+# # DANGER! This is insecure. See http://twil.io/secure
+# account_sid = 'AC952660eb8a5a24050f08e7c6de594f18'
+# auth_token = '14b1a8d9b7e8f360caecae7bec9fec7b'
+# client = Client(account_sid, auth_token)
+
+# message1 = ""
+
+# recipients = ["+19252315793", "+16266784605", "+13105005484", "+16267717083",
+#               "+16262286133", "+17148555744", "+14806164894", "+16193840483", "+19167573132"]
+
+# for recipient in recipients:
+#     client.messages.create(body=message1, from_='+14159656427', to=recipient)
+
+temp = "racecar"
